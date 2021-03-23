ce69b19b33c45ff4043833dd29a98fbe95e7c32e
HenrikSkog <55615149+HenrikSkog@users.noreply.github.com>
2021-03-23 10:57:56 +0100

    new post

--- prosjektoppgaveTDT4145/src/main/java/org/dbprosjekt/controllers/NewPostController.java
+++ prosjektoppgaveTDT4145/src/main/java/org/dbprosjekt/controllers/NewPostController.java
@@ -1,14 +1,16 @@
 package org.dbprosjekt.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 import org.dbprosjekt.App;
 import org.dbprosjekt.database.DatabaseQueryGenerator;
 
+import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class NewPostController {
 	@FXML
@@ -18,15 +20,43 @@ public class NewPostController {
 	TextArea textInput;
 
 	@FXML
-	ToggleGroup tag;
+	ChoiceBox tags;
 
 	@FXML
-	public void newPost() {
+	Pane container;
+
+	@FXML
+	CheckBox anonymousBox;
+
+	@FXML
+	public void newPost() throws SQLException {
 		var queryGenerator = new DatabaseQueryGenerator();
 
-		var selectedTag = (RadioButton) tag.getSelectedToggle();
-		var tagVal = selectedTag.getText();
+		var selectedTag = tags.getValue().toString();
 
-		queryGenerator.insertThreadPost(titleInput.getText(), textInput.getText(), tagVal);
+		int isAnonymous = 0;
+
+		if(anonymousBox.isSelected()) {
+			isAnonymous = 1;
+		}
+
+		queryGenerator.insertThreadPost(titleInput.getText(), textInput.getText(), selectedTag, isAnonymous);
+
+		Program2Controller.reload();
+	}
+
+	@FXML
+	public void initialize() {
+		var queryGenerator = new DatabaseQueryGenerator();
+		boolean allowsAnon = queryGenerator.currentCourseAllowsAnonymous();
+
+		if(!allowsAnon) {
+			anonymousBox.setDisable(true);
+		}
+
+		tags.setItems(
+			FXCollections.observableArrayList(
+				"Question", "Homework ", "Homework solution", "Lectures notes", "General announcement"
+		));
 	}
 }
--- prosjektoppgaveTDT4145/src/main/java/org/dbprosjekt/database/DatabaseQueryGenerator.java
+++ prosjektoppgaveTDT4145/src/main/java/org/dbprosjekt/database/DatabaseQueryGenerator.java
@@ -3,170 +3,177 @@ package org.dbprosjekt.database;
 import org.dbprosjekt.controllers.Program2Controller;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class DatabaseQueryGenerator extends DBConn {
-    public DatabaseQueryGenerator() {
-        super();
-    }
+	public DatabaseQueryGenerator() {
+		super();
+	}
 
 
-    public ResultSet query(String queryString) {
-        try {
-            Statement statement = conn.createStatement();
-            return statement.executeQuery(queryString);
-        } catch (Exception e) {
-            System.out.println(e.getCause());
-            return null;
-        }
-    }
+	public ResultSet query(String queryString) {
+		try {
+			Statement statement = conn.createStatement();
+			return statement.executeQuery(queryString);
+		} catch (Exception e) {
+			System.out.println(e.getCause());
+			return null;
+		}
+	}
 
-    public ArrayList<String> getSelectResult(ResultSet rs, String... properties) {
-        var res = new ArrayList<String>();
-        try {
-            while (rs.next()) {
-                Arrays.asList(properties).forEach(property -> {
-                    try {
-                        res.add(rs.getString(property));
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return res;
-    }
+	public ArrayList<String> getSelectResult(ResultSet rs, String... properties) {
+		var res = new ArrayList<String>();
+		try {
+			while (rs.next()) {
+				Arrays.asList(properties).forEach(property -> {
+					try {
+						res.add(rs.getString(property));
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+				});
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return res;
+	}
 
-    public boolean queryHasResultRows(String queryString) {
-        var rs = query(queryString);
-        if (rs==null)
-            return false;
-        try {
-            if (rs.isBeforeFirst()) {
-                return true;
-            }
-            return false;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public boolean queryHasResultRows(String queryString) {
+		var rs = query(queryString);
+		if (rs==null)
+			return false;
+		try {
+			if (rs.isBeforeFirst()) {
+				return true;
+			}
+			return false;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-		public String getLastInsertedID() {
-        try {
-            String query = "select LAST_INSERT_ID() as id";
-            var rs = query(query);
-            var data = getSelectResult(rs, "id");
+	public boolean currentCourseAllowsAnonymous(){
+		try {
+			String queryString = "select * from Course where SubjectID='" + Session.getCourseID() + "' and Term='" + Session.getTerm() + "'";
+			var rs = query(queryString);
+			var data = getSelectResult(rs, "AllowsAnonymous");
+			if(data.get(0) == "1") {
+				return true;
+			}
+		} catch(Exception e) {
+			System.out.println("exception in check for anonymous");
+			System.out.println(e);
+		}
+		return false;
+	}
 
-            if(data.size() == 0) {
-                return null;
-            }
+	public String getLastInsertedID() {
+		try {
+			String query = "select LAST_INSERT_ID() as id";
+			var rs = query(query);
+			var data = getSelectResult(rs, "id");
 
-            return data.get(0);
-        } catch (Exception e) {
-            System.out.println(e);
-            return null;
-        }
-    }
+			if(data.size() == 0) {
+				return null;
+			}
 
-    public String buildInsert(String table, String... values) {
-        String start = "insert into " + table + " values(";
-        for(String val: Arrays.asList(values)) {
-            if(val == null) {
-                start += "null,";
-            } else if(val == "CURDATE()" || val == "CURTIME()" || val.matches("-?\\d+(\\.\\d+)?")) {
-                start += val + ",";
-            } else {
-            		start += "'" + val + "',";
-            }
-        };
-        start = start.substring(0, start.length()-1) + ")";
-        return start;
-    }
+			return data.get(0);
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
 
-    public void insertUser(String email, String username, String password) {
-        try {
-            Statement statement = conn.createStatement();
-            String queryString = "insert into User(Email, Username, Password, Type) values('" + email + "','" + username + "','" + password + "','student')";
-            statement.execute(queryString);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+	public String buildInsert(String table, String... values) {
+		String start = "insert into " + table + " values(";
+		for(String val: Arrays.asList(values)) {
+			if(val == null) {
+				start += "null,";
+			} else if(val == "CURDATE()" || val == "CURTIME()" || val.matches("-?\\d+(\\.\\d+)?")) {
+				start += val + ",";
+			} else {
+				start += "'" + val + "',";
+			}
+		};
+		start = start.substring(0, start.length()-1) + ")";
+		return start;
+	}
 
-    public void insertThreadPost(String title, String text, String tag) {
-        try {
-            var statement = conn.createStatement();
+	public void insertUser(String email, String username, String password) {
+		try {
+			Statement statement = conn.createStatement();
+			String queryString = "insert into User(Email, Username, Password, Type) values('" + email + "','" + username + "','" + password + "','student')";
+			statement.execute(queryString);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
-            String queryString = "select * from Course where SubjectID='" + Session.getCourseID() + "' and Term='" + Session.getTerm() + "'";
-            var rs = query(queryString);
-            var isAnonymous = getSelectResult(rs, "AllowsAnonymous");
+	public void insertThreadPost(String title, String text, String tag, int isAnonymous) {
+		try {
+			var statement = conn.createStatement();
 
 //            insert post row
-            String insertPostQueryString = buildInsert("Post", null, text, "CURDATE()", "CURTIME()", isAnonymous.get(0), Session.getUserID());
-            statement.execute(insertPostQueryString);
+			String insertPostQueryString = buildInsert("Post", null, text, "CURDATE()", "CURTIME()", Integer.toString(isAnonymous), Session.getUserID());
+			statement.execute(insertPostQueryString);
 
 //            get row id
-            String id = getLastInsertedID();
+			String id = getLastInsertedID();
 
 //            insert thread post
-            String insertThreadPostQueryString = buildInsert("ThreadPost", id, tag, title);
-            statement.execute(insertThreadPostQueryString);
+			String insertThreadPostQueryString = buildInsert("ThreadPost", id, tag, title);
+			statement.execute(insertThreadPostQueryString);
 
 
 //            insert thread in folder
-            String insertThreadInFolderQueryString = buildInsert("ThreadInFolder", Integer.toString(Session.getCurrentFolderID()), id);
+			String insertThreadInFolderQueryString = buildInsert("ThreadInFolder", Integer.toString(Session.getCurrentFolderID()), id);
 
-            System.out.println("querystring: " + insertThreadInFolderQueryString);
+			statement.execute(insertThreadInFolderQueryString);
 
-            statement.execute(insertThreadInFolderQueryString);
-
-        } catch (Exception e) {
-            System.out.println("exeptioooon");
-            System.out.println(e);
-            System.out.println(e.getCause());
-            System.out.println(e.getMessage());
-            System.out.println(e.getStackTrace());
-        }
-    }
+		} catch (Exception e) {
+			System.out.println("exception in post insert");
+			System.out.println(e.getMessage());
+		}
+	}
 
 
-    public void insertSubject(String id, String name){
-        try {
-            Statement statement = conn.createStatement();
-            String queryString = "insert into Subject(SubjectID, name ) values('" + id + "','" + name + "')";
-            statement.execute(queryString);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-    public void insertCourse(String id, String term, boolean allowsAnonymous){
-        try {
-            Statement statement = conn.createStatement();
-            String queryString = "insert into Course(SubjectID, Term, AllowsAnonymous) values('" + id + "','" + term + "',"+allowsAnonymous+")";
-            statement.execute(queryString);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-    public void insertFolder(String name){
-        String queryString = "";
-        try {
-            String term = Session.getTerm();
-            String courseID = Session.getCourseID();
-            Statement statement = conn.createStatement();
-            if(Session.getCurrentFolderID()==0){
-                queryString = "insert into Folder(FolderID,Name,ParentID,SubjectID,Term) values(null,'"+name+"',null,'"+courseID+"','"+term+"')";
-            }
-            else{
-                queryString = "insert into Folder(FolderID,Name,ParentID,SubjectID,Term) values(null,'"+name+"','"+Session.getCurrentFolderID()+"',null,null)";
-            }
-            statement.execute(queryString);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+	public void insertSubject(String id, String name){
+		try {
+			Statement statement = conn.createStatement();
+			String queryString = "insert into Subject(SubjectID, name ) values('" + id + "','" + name + "')";
+			statement.execute(queryString);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	public void insertCourse(String id, String term, boolean allowsAnonymous){
+		try {
+			Statement statement = conn.createStatement();
+			String queryString = "insert into Course(SubjectID, Term, AllowsAnonymous) values('" + id + "','" + term + "',"+allowsAnonymous+")";
+			statement.execute(queryString);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	public void insertFolder(String name){
+		String queryString = "";
+		try {
+			String term = Session.getTerm();
+			String courseID = Session.getCourseID();
+			Statement statement = conn.createStatement();
+			if(Session.getCurrentFolderID()==0){
+				queryString = "insert into Folder(FolderID,Name,ParentID,SubjectID,Term) values(null,'"+name+"',null,'"+courseID+"','"+term+"')";
+			}
+			else{
+				queryString = "insert into Folder(FolderID,Name,ParentID,SubjectID,Term) values(null,'"+name+"','"+Session.getCurrentFolderID()+"',null,null)";
+			}
+			statement.execute(queryString);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
