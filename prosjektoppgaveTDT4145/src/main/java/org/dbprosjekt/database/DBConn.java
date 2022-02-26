06070e675a7da640d26a5eb47e1956f12778dd08
Henrik Skog <henrikskog01@gmail.com>
2022-02-26 23:24:25 +0100

    fix: use local database

--- prosjektoppgaveTDT4145/src/main/java/org/dbprosjekt/database/DBConn.java
+++ prosjektoppgaveTDT4145/src/main/java/org/dbprosjekt/database/DBConn.java
@@ -17,14 +17,14 @@ public class DBConn {
 			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 			var dbConnString = new StringBuilder();
 
-			dbConnString.append("jdbc:mysql://79.160.125.77:3306/");
-			dbConnString.append("halvor_prosjekt");
+			dbConnString.append("jdbc:mysql://localhost:3306/");
+			dbConnString.append("testhenrik");
 			dbConnString.append("?useSSL=false");
 			dbConnString.append("&allowPublicKeyRetrieval=true");
 			dbConnString.append("&serverTimezone=Europe/Oslo");
 
 
-			conn = DriverManager.getConnection(dbConnString.toString(), "halvor", "O^o]FcUwpHbIQ=^KXL!%HW6I");
+			conn = DriverManager.getConnection(dbConnString.toString(), "root", "test");
 		} catch (Exception e) {
 			System.out.println(e);
 			throw new RuntimeException("Unable to connect", e);
